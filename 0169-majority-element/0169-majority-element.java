999b29c4cbdb1930878d441786d3d47bc4995b7c
Shubham Kumar Jha <shubhamgvps.05@gmail.com>
2026-03-20 18:47:23 +0530

    Time: 1 ms (99.83%), Space: 55.2 MB (73.49%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+class Solution {
+    public static int majorityElement(int[] nums) {
+        int count = 0;
+        int candidate = 0;
+
+        for (int num : nums) {
+            if (count == 0) {
+                candidate = num;
+            }
+
+            if (num == candidate) {
+                count++;
+            } else {
+                count--;
+            }
+        }
+
+        return candidate;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.println(majorityElement(arr));
+    }
+}
+
